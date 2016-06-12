35f333bf408d50922fa1dd01b701c3cc544f5aa8
Hiroyuki Tsuruda <mocchicc@me.com>
2016-06-12 13:59:40 +0900

    UITextFieldDelegate, commentTextField.resignFirstResponder()

--- TechCamp-NicoNicoApp/ViewController.swift
+++ TechCamp-NicoNicoApp/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var commentTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        commentTextField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +23,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapSubmitButton(sender: UIButton) {
+        commentTextField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        commentTextField.resignFirstResponder()
+        return true
+    }
 
 }
 

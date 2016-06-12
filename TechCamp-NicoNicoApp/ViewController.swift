529da457adffa30aa8dbb8eaa69921e27c6e49d9
Hiroyuki Tsuruda <mocchicc@me.com>
2016-06-12 13:35:46 +0900

    Initial Commit

---
+++ TechCamp-NicoNicoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TechCamp-NicoNicoApp
+//
+//  Created by mocchicc on 2016/06/12.
+//  Copyright © 2016年 mocchicc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

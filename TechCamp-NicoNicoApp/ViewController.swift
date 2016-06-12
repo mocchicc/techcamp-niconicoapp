37eea199ffbde3f5692a04cbf625f1c68acefbd9
Hiroyuki Tsuruda <mocchicc@me.com>
2016-06-12 14:06:38 +0900

    label.text = commentTextField.text

--- TechCamp-NicoNicoApp/ViewController.swift
+++ TechCamp-NicoNicoApp/ViewController.swift
@@ -26,8 +26,14 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBAction func tapSubmitButton(sender: UIButton) {
         commentTextField.resignFirstResponder()
         let label = UILabel()
+        
+        //commentTextFieldの内容を取得
+        label.text = commentTextField.text
+        
         label.frame.origin = CGPoint(x: 130, y: 200)
         label.font = UIFont(name: "HirakakuProN-W6", size:20)
+        
+        //サイズの設定は、テキスト内容を決めてから記述する
         label.sizeToFit()
     }
     

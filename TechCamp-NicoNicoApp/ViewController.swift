8ce6ed555072c40205c864d78e2c1b9674f4aa8d
Hiroyuki Tsuruda <mocchicc@me.com>
2016-06-12 14:30:20 +0900

    コメントを動かすアニメーションを実装

--- TechCamp-NicoNicoApp/ViewController.swift
+++ TechCamp-NicoNicoApp/ViewController.swift
@@ -29,7 +29,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
         //commentTextFieldの内容を取得
         label.text = commentTextField.text
         
-        label.frame.origin = CGPoint(x: 130, y: 200)
+        //ラベルの位置が動くように設定
+        //ラベルの位置(X)を画面の幅と同じにする事でかならず画面の外にラベルを配置できる
+        label.frame.origin = CGPoint(x: self.view.frame.width, y: 200)
+        
         label.font = UIFont(name: "HirakakuProN-W6", size:20)
         
         //サイズの設定は、テキスト内容を決めてから記述する
@@ -44,11 +47,15 @@ class ViewController: UIViewController, UITextFieldDelegate {
         //生成したLabelをViewControllerに表示
         self.view.addSubview(commentLabel)
         
-        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(ViewController.moveComment(_:)), userInfo: nil, repeats: true)
+        NSTimer.scheduledTimerWithTimeInterval(0.1, target: self, selector: #selector(ViewController.moveComment(_:)), userInfo: commentLabel, repeats: true)
     }
 
     func moveComment(sender: NSTimer) {
-        print("Hello World")
+        let commentLabel = sender.userInfo as! UILabel
+        //userInfoに指定したcommentLabelはmoveCommet()メソッドの引数であるsenderから取得している
+        //commetLabelのx座標はframe.origin.xで取得
+        //また、-=演算子を用いることでx座標の値を20pxずつ減らす
+        commentLabel.frame.origin.x -= 20
     }
     
     func textFieldShouldReturn(textField: UITextField) -> Bool {
